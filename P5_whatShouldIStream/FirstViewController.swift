4e2e0712aa009fe48c0a5d5ca0cd953a91f01a93
mharpe2 <mharpe2@gmail.com>
2016-04-11 22:31:19 -0400

    Initial Commit

---
+++ P5_whatShouldIStream/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  P5_whatShouldIStream
+//
+//  Created by Michael Harper on 4/11/16.
+//  Copyright © 2016 MJH. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ P5_whatShouldIStream/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  P5_whatShouldIStream
+//
+//  Created by Michael Harper on 4/11/16.
+//  Copyright © 2016 MJH. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
